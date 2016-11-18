e57be9037af24a15da619c089fe6ca903f87f94b
atena <ashenatena@gmail.com>
2016-11-18 10:59:30 +0330

    using strip and ommiting newline

--- database.py
+++ database.py
@@ -3,13 +3,13 @@ import csv
 
 def create_database(name):
     try:
-        database = open(name+'.csv', 'w', newline='')
+        database = open(name+'.csv', 'w')
     except IOError:
         database.close()
 
 def use_database(name):
     global table
-    database = open(name+'.csv', 'a', newline='')
+    database = open(name+'.csv', 'a')
     table = csv.reader(database)
     while True:
         line = sys.stdin.readline()
@@ -17,23 +17,23 @@ def use_database(name):
             t = command(line.split(' '))
             if t == 1:
                 database.close()
-                database = open(name+'.csv', 'r', newline='')
+                database = open(name+'.csv', 'r')
                 table = csv.reader(database)
                 for row in table:
-                    if row == [line.split(' ')[2].split(';\n')[0]]:
+                    if row == [line.strip(';\n').split(' ')[2]]:
                         t = 0
                 database.close()
-                database = open(name+'.csv', 'a', newline='')
+                database = open(name+'.csv', 'a')
                 if t == 1:
                     table = csv.writer(database)
                     table.writerow([table_name])
                     table.writerow(col_name)
             if t == 2:
                 database.close()
-                database = open(name+'.csv', 'r', newline='')
+                database = open(name+'.csv', 'r')
                 table = csv.reader(database)
                 for row in table:
-                    if row == [line.split(' ')[2].split(';\n')[0]]:
+                    if row == [line.strip(';\n').split(' ')[2]]:
                         for i in table.__next__():
                             print(i)
         else:
@@ -44,16 +44,15 @@ def create_table(com):
     global table_name
     table_name = com[0]
     global col_name
-    com[-1] = com[-1].split(';\n')[0]
     com = [col.split(',')[0] for col in com] 
     col_name = com[2:]    
 
 def command(line):
     if line[0] == "CREATE" and line[1] == "DATABASE":
-        create_database(line[2].split(';\n')[0])
+        create_database(line[2])
         return 0
     elif line[0] == "USE" and line[1] == "DATABASE":
-        use_database(line[2].split(';\n')[0])
+        use_database(line[2])
         return 0
     elif line[0] == "CREATE" and line[1] == "TABLE":
         create_table(line[2:])
@@ -65,6 +64,6 @@ while True:
     global database
     line = sys.stdin.readline()
     if line:
-       command(line.split(' '))
+       command(line.strip(';\n').split(' '))
     else:
         sys.exit(0)
