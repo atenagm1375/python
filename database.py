da01d2f0c5b82d91d56c0e0b96fc98103e56cc05
atena <ashenatena@gmail.com>
2016-11-20 15:45:03 +0330

    INSERT INTO added

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sys
 import csv
+import os
+import shutil
 
 def create_database(name):
     try:
@@ -9,6 +11,7 @@ def create_database(name):
 
 def use_database(name):
     global table
+<<<<<<< Updated upstream
     with open(name+'.csv', 'a', newline='') as database:
         table = csv.reader(database)
         while True:
@@ -53,6 +56,53 @@ def use_database(name):
             else:
                 sys.exit(0)
 
+=======
+    database = open(name+'.csv', 'a')
+    table = csv.reader(database)
+    while True:
+        line = sys.stdin.readline()
+        if line:
+            t = command(line.strip(';\n').split(' '))
+            if t == 1:
+                database.close()
+                database = open(name+'.csv', 'r')
+                table = csv.reader(database)
+                for row in table:
+                    if row == [line.strip(';\n').split(' ')[2]]:
+                        t = 0
+                database.close()
+                database = open(name+'.csv', 'a')
+                if t == 1:
+                    table = csv.writer(database)
+                    table.writerow([table_name])
+                    table.writerow(col_name)
+            if t == 2:
+                database.close()
+                database = open(name+'.csv', 'r')
+                table = csv.reader(database)
+                for row in table:
+                    if row == [line.strip(';\n').split(' ')[2]]:
+                        for i in table.__next__():
+                            print(i)
+            if t == 3:
+                database.close()
+                database = open(name+'.csv', 'r')
+                f = open(name+'.csv.temp', 'w')
+                tableR = csv.reader(database)
+                tableW = csv.writer(f)
+                for row in tableR:
+                    print(row)
+                    tableW.writerow(row)
+                    if row == [line.strip(';\n').split(' ')[2]]:
+                        tableW.writerow(tableR.__next__())
+                        tableW.writerow(line.strip(';\n').replace(', ', ' ').split(' ')[4:])
+                f.close()
+                database.close()
+                shutil.move(name+'.csv.temp', name+'.csv')
+        else:
+            sys.exit(0)
+    database.close()
+>>>>>>> Stashed changes
 
 def create_table(com):
     global table_name
@@ -60,7 +110,7 @@ def create_table(com):
     global col_name
     com[-1] = com[-1].split(';\n')[0]
     com = [col.split(',')[0] for col in com] 
-    col_name = com[2:]    
+    col_name = com[2:]
 
 def command(line):
     if line[0] == "CREATE" and line[1] == "DATABASE":
